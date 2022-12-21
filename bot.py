213df922b63471e3f943d67dda5eccdcb99cd794
Alex None <lex.dev@aol.com>
2022-12-21 22:37:36 +0300

    init

---
+++ bot.py
@@ -0,0 +1,31 @@
+import time
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN = os.environ.get('TOKEN')
+MSG = "Did you program today, {}?"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot=bot)
+
+logging.basicConfig(level=logging.INFO)
+
+
+@dp.message_handler(commands=['start'])
+async def start_handler(message: types.Message):
+    USER_ID = message.from_user.id
+    USER_NAME = message.from_user.first_name
+    USER_FULL_NAME = message.from_user.full_name
+    logging.info(f'{USER_ID=} {USER_FULL_NAME=} {time.asctime()}')
+
+    await message.reply(f"Hello, {USER_FULL_NAME}! Don't you think it's a good day?)")
+
+    for i in range(7):
+        time.sleep(60*60*24)
+
+        await bot.send_message(USER_ID, MSG.format(USER_NAME))
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
